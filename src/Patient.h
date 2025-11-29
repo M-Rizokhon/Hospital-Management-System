3d6eb19391a58492d9fc3c6767e9252576cb6149
Muhammad Rizo <rizohonrahimjonov@gmail.com>
2025-11-29 13:35:51 +0500

    Added a patient class

---
+++ src/Patient.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <string>
+// implementation of a patient class
+
+
+
+class Patient {
+private:
+	std::string name;
+	int severity;
+	int id;
+
+
+public:
+	// constructor
+	Patient(std::string name, int severity, int id) : name(name), severity(severity), id(id) {}
+
+	std::string getName() const { return name; }
+	void setName(std::string new_name) { name = new_name; }
+
+	int getID() const { return id; }
+	void setID(int new_id) { id = new_id; }
+
+    int getSeverity() const { return severity; }
+    void setSeverity(int sev) { severity = sev; }
+
+
+	void info() const;
+
+};
+
+
+
