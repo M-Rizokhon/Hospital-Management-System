a11dd4966a7697b2d81ca3edad9669771d56103e
Muhammad Rizo <rizohonrahimjonov@gmail.com>
2025-12-03 22:02:55 +0500

    Corrected the file structure

--- Hospital.h
+++ Hospital.h
@@ -1,10 +1,10 @@
 #pragma once
 #include <iostream>
 #include "Patient.h"
-#include "../BST.h"
-#include "../CircularQueue.h"
-#include "../LinkedList.h"
-#include "../Hash_map.h"
+#include "data_structures/BST.h"
+#include "data_structures/CircularQueue.h"
+#include "data_structures/LinkedList.h"
+#include "data_structures/Hash_map.h"
 
 class Hospital {
 private:
